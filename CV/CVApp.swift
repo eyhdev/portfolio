24aa21953e9f775f57c962142a0d7b9ba0e897b1
eyhdev <alaqsa335@gmail.com>
2023-09-10 12:15:15 +0300

    Initial Commit

---
+++ CV/CVApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVApp.swift
+//  CV
+//
+//  Created by eyh.mac on 10.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
